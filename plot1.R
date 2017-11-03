aa096a8a85eb7ca435471286ad30cc3a46a6aae5
galshiba <aaron.galluzzi@sympatico.ca>
2017-11-02 20:06:57 -0400

    All files

--- plot1.R
+++ plot1.R
@@ -7,5 +7,5 @@ subdata <- household[household$Date %in% c("1/2/2007","2/2/2007"),]
 
 # Not plot the Global Active Power
 png("plot1.png",width=480,height=480)
-hist(as.numeric(subdata$Global_active_power),col="red",main="Global Active Power (kilowatts)")
+hist(as.numeric(subdata$Global_active_power),col="red",main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off()
